a1373a0b136375e1fa631809c1316dffefdfd784
Stefan Seefeld <stefan@codesourcery.com>
2013-12-03 14:00:31 -0500

    Fix issue 2

--- vsipl/code/boolean.h
+++ vsipl/code/boolean.h
@@ -1,5 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
 <synopsis version="5.0" xmlns="http://docbook.org/ns/docbook">
-typedef unsigned int vsip_scalar_bl;
+typedef <emphasis revisionflag="changed">implementation-defined</emphasis> vsip_scalar_bl;
 typedef vsip_scalar_bl vsip_bool;
 #define VSIP_FALSE 0
 #define VSIP_TRUE 1
